ea403e0c1fbeea304b5d31e999130f2a34506589
Jiri Kraml <jiri.kraml@mailbox.tu-dresden.de>
2016-10-02 19:54:46 +0200

    move rendering to own class

--- src/main/scala/eu/kraml/Main.scala
+++ src/main/scala/eu/kraml/Main.scala
@@ -3,10 +3,9 @@ package eu.kraml
 import java.io.File
 import java.time.Instant
 
-import eu.kraml.Constants.{MAX_ZOOM, MIN_ZOOM, TILE_WIDTH}
 import eu.kraml.io.{GpxFileReader, MainConfigReader, RenderConfigReader, TileCache}
-import eu.kraml.model.{InvocationConfig, PointStyle, Record, RenderConfig}
-import eu.kraml.render.MapCanvas
+import eu.kraml.model.{InvocationConfig, Record, RenderConfig}
+import eu.kraml.render.Renderer
 
 import scala.collection.mutable
 
@@ -24,10 +23,11 @@ object Main {
         val renderConfigs: Map[String, (RenderConfig, Instant)]= readRenderConfigs(mainConfig.configDir)
         warnIfSameOutputFilesAreUsed(renderConfigs)
 
+        val renderer = new Renderer(cache, mainConfigModificationDate, mainConfig.outputDir, invocationConfig.forceRender)
         renderConfigs foreach {
             case (name, (conf, modDate)) =>
                 println("processing " + name)
-                render(records, conf, cache, mainConfig.outputDir, modDate, invocationConfig.forceRender)
+                renderer.render(records, conf, modDate)
         }
     }
 
@@ -64,68 +64,6 @@ object Main {
                     RenderConfigReader.readRenderConfig(f).map(c => f.getName -> (c, modDate))
             })
             .toMap
-    }
-
-    //TODO move render method to own class (use one instance for each output file)
-    private def render(records: Iterable[Record], conf: RenderConfig, cache: TileCache, outputDir: File, configModificationDate: Instant, forceRender: Boolean = true): Unit = {
-        val outfile = new File(outputDir, conf.outputFileName)
-        val bBox = conf.boundingBox
-        val recordsInBBox = records.filter(r => bBox.contains(r.coordinate)).toList
-        val recordsAndStyles = new mutable.ListBuffer[(List[Record], PointStyle)]()
-        var mostRecentRelevantDate = configModificationDate
-
-        conf.groups.foreach( groupConf => {
-            val matchingRecords = recordsInBBox
-                .filter(groupConf.filter.recordMatches)
-            recordsAndStyles.append((matchingRecords, groupConf.style))
-
-            val newestRecordDate = matchingRecords.map(_.timestamp).sorted.reverse.head
-            mostRecentRelevantDate = mostRecent(mostRecentRelevantDate, newestRecordDate)
-        })
-
-        val lastRenderDate = Instant.ofEpochSecond(outfile.lastModified())
-        if (!forceRender && (mostRecentRelevantDate isBefore lastRenderDate)) {
-            println("nothing has changed since last rendering")
-            return
-        }
-
-        val zoom = findZoom(bBox.width, conf.targetWidth)
-        println("chose zoom " + zoom)
-        val canvas = new MapCanvas(cache, bBox, zoom)
-
-        recordsAndStyles.foreach {
-            case (filteredRecords, style) =>
-                filteredRecords
-                    .map(_.coordinate)
-                    .foreach(c => canvas.addPoint(c, style))
-        }
-
-        canvas.render.output(outfile)
-    }
-
-    private def mostRecent(instants: Instant*): Instant = {
-        instants.reduce((i1, i2) => {
-            if (i1 isAfter i2)
-                i1
-            else
-                i2
-        })
-    }
-
-    private def findZoom(widthInDegrees: Double, targetWidthInPx: Int):Int = {
-        val targetWidthInTiles = targetWidthInPx / TILE_WIDTH
-        val widthInPercentOfWorld = widthInDegrees / 360.0
-
-        val candidates: List[(Int, Double)] =
-        (MIN_ZOOM to MAX_ZOOM).map(z => {
-            val width = math.pow(2,z) * TILE_WIDTH * widthInPercentOfWorld
-            val error = width - targetWidthInPx
-            (z, error)
-        }).toList
-
-        val sortedCandidates = candidates.sortBy {case (zoom, error) => math.pow(error, 2)} // quadratic error
-
-        sortedCandidates.head._1
     }
 
 }
---
+++ src/main/scala/eu/kraml/render/Renderer.scala
@@ -0,0 +1,77 @@
+package eu.kraml.render
+
+import java.io.File
+import java.time.Instant
+
+import eu.kraml.Constants._
+import eu.kraml.io.TileCache
+import eu.kraml.model.{PointStyle, Record, RenderConfig}
+import eu.kraml.render.Renderer.{findZoom, mostRecent}
+
+import scala.collection.mutable
+
+class Renderer(val cache: TileCache, val mainConfigModificationDate: Instant, val outputDir: File, var forceRender: Boolean = true) {
+
+    def render(records: Iterable[Record], conf: RenderConfig, configModificationDate: Instant): Unit = {
+        val outfile = new File(outputDir, conf.outputFileName)
+        val bBox = conf.boundingBox
+        val recordsInBBox = records.filter(r => bBox.contains(r.coordinate)).toList
+        val recordsAndStyles = new mutable.ListBuffer[(List[Record], PointStyle)]()
+        var mostRecentRelevantDate = mostRecent(mainConfigModificationDate, configModificationDate)
+
+        conf.groups.foreach( groupConf => {
+            val matchingRecords = recordsInBBox
+                .filter(groupConf.filter.recordMatches)
+            recordsAndStyles.append((matchingRecords, groupConf.style))
+
+            val newestRecordDate = matchingRecords.map(_.timestamp).sorted.reverse.head
+            mostRecentRelevantDate = mostRecent(mostRecentRelevantDate, newestRecordDate)
+        })
+
+        val lastRenderDate = Instant.ofEpochSecond(outfile.lastModified()) //also covers the case where output file does not exist
+        if (!forceRender && (mostRecentRelevantDate isBefore lastRenderDate)) {
+            println("nothing has changed since last rendering")
+            return
+        }
+
+        val zoom = findZoom(bBox.width, conf.targetWidth)
+        val canvas = new MapCanvas(cache, bBox, zoom)
+
+        recordsAndStyles.foreach {
+            case (filteredRecords, style) =>
+                filteredRecords
+                    .map(_.coordinate)
+                    .foreach(c => canvas.addPoint(c, style))
+        }
+
+        canvas.render.output(outfile)
+    }
+}
+
+object Renderer {
+
+    private def mostRecent(instants: Instant*): Instant = {
+        instants.reduce((i1, i2) => {
+            if (i1 isAfter i2)
+                i1
+            else
+                i2
+        })
+    }
+
+    private def findZoom(widthInDegrees: Double, targetWidthInPx: Int):Int = {
+        val targetWidthInTiles = targetWidthInPx / TILE_WIDTH
+        val widthInPercentOfWorld = widthInDegrees / 360.0
+
+        val candidates: List[(Int, Double)] =
+            (MIN_ZOOM to MAX_ZOOM).map(z => {
+                val width = math.pow(2,z) * TILE_WIDTH * widthInPercentOfWorld
+                val error = width - targetWidthInPx
+                (z, error)
+            }).toList
+
+        val sortedCandidates = candidates.sortBy {case (zoom, error) => math.pow(error, 2)} // quadratic error
+
+        sortedCandidates.head._1
+    }
+}
