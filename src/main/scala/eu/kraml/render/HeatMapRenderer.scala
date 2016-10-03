df4ecdc03de4bd411e347cfdf4ce73eff9e9ed4d
Jiri Kraml <jiri.kraml@mailbox.tu-dresden.de>
2016-10-03 23:11:25 +0200

    groups points together in heat map renderer to improve performance

--- src/main/scala/eu/kraml/render/HeatMapRenderer.scala
+++ src/main/scala/eu/kraml/render/HeatMapRenderer.scala
@@ -5,31 +5,43 @@ import java.awt.image.BufferedImage
 
 import eu.kraml.Main.ProgressMonitor
 import eu.kraml.model.Record
+import eu.kraml.render.MapCanvas.PxCoord
 
 
 private[render] class HeatMapRenderer() extends RecordRenderer {
     override def render(overlay: BufferedImage, coordinateConverter: CoordinateConverter, records: List[Record])
                        (implicit progress: ProgressMonitor): Unit = {
         val process = progress.registerProcess("rendering heatmap pixel")
-        process.setMaxValue(overlay.getWidth*overlay.getHeight)
+        process.setMaxValue(overlay.getWidth * overlay.getHeight)
 
-        //TODO use some kind of index structure for records, the iterations are very slow
+        val pointWeights: Map[PxCoord, Int] = records
+            .map(_.coordinate)
+            .map(coordinateConverter.toCanvasCoords)
+            .groupBy(identity)
+            .mapValues(_.size)
+
+        //TODO use some kind of 2D index structure for point weights, the iterations are very slow
         val distanceCutoff = 80.0
         val exponent = 3
         for (y <- 0 until overlay.getHeight;
              x <- 0 until overlay.getWidth) {
-            val weight = records.map(_.coordinate)
-                .map(coordinateConverter.toCanvasCoords)
-                .map(c => math.sqrt(math.pow(c.x - x, 2) + math.pow(c.y - y, 2)))
-                .filter(_ < distanceCutoff)
+            val weight = pointWeights
+                .map( {
+                    case (coord, count) => (math.sqrt(math.pow(coord.x - x, 2) + math.pow(coord.y - y, 2)), count)
+                })
+                .filter( {
+                    case (distance, count) => distance < distanceCutoff
+                })
                 .map {
-                    d  => (distanceCutoff-d)/distanceCutoff
+                    case (distance, count) =>
+                        val linearFactor = (distanceCutoff - distance) / distanceCutoff
+                        val modifiedFactor = math.pow(linearFactor, exponent)
+                        count * modifiedFactor
                 }
-                .map (math.pow(_, exponent))
                 .sum
             val clipped = math.min(weight, 255).toInt
-            val color = new Color(255,0,0, clipped)
-            overlay.setRGB(x,y,color.getRGB)
+            val color = new Color(255, 0, 0, clipped)
+            overlay.setRGB(x, y, color.getRGB)
             process << 1
         }
 
